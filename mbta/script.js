ba2957060696742673493b1aca69b9c6a2591ffa
Garrett Hale <garretthale@Michaels-MacBook-Pro.local>
2018-10-10 17:26:24 -0400

    added first marker

--- mbta/script.js
+++ mbta/script.js
@@ -1,7 +1,15 @@
+var DavisLatLng = {lat: 42.396797, lng: -71.121873};
+
 var map;
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: -34.397, lng: 150.644},
+    center: {lat: 42.3501, lng: -71.0589},
     zoom: 8
   });
+
+var davismarker = new google.maps.Marker({
+    position: DavisLatLng,
+    map: map,
+    title: 'Davis Square'
+        });
 }
